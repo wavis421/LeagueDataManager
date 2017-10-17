0103b2ac2d6b8410dad350086695761ff140013f
wavis421 <wendy.avis@jointheleague.org>
2017-10-16 19:47:04 -0700

    First attempt at getting Github API working (not yet implemented for github classroom)
    1) Still need to implement for Github classroom.
    2) Do not make this the default solution until we figure out how to get commits "since". Otherwise the GET is way too broad. Query to github help in progress.

--- src/model/GitApiController.java
+++ src/model/GitApiController.java
@@ -3,6 +3,7 @@ package model;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.json.Json;
 import javax.json.JsonArray;
@@ -11,6 +12,14 @@ import javax.json.JsonObject;
 import javax.json.JsonReader;
 import javax.json.JsonStructure;
 import javax.json.stream.JsonParsingException;
+import javax.swing.JOptionPane;
+
+import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.RepositoryCommit;
+import org.eclipse.egit.github.core.client.GitHubClient;
+import org.eclipse.egit.github.core.service.CommitService;
+import org.eclipse.egit.github.core.service.RepositoryService;
+import org.joda.time.DateTime;
 
 public class GitApiController {
 	// Repo name format: level-X-module-Y-username
@@ -20,12 +29,119 @@ public class GitApiController {
 	// TODO: Get this from a file or website
 	private static final String token = "223bcb4816e95309f88c1154377f721e3d77568a";
 	private MySqlDatabase sqlDb;
+	private RepositoryService repoService;
+	private CommitService commitService;
 
 	public GitApiController(MySqlDatabase sqlDb) {
 		this.sqlDb = sqlDb;
+
+		// OAuth2 token authentication
+		GitHubClient client = new GitHubClient();
+		client.setOAuth2Token(token);
+
+		// Get Repo and Commit services
+		repoService = new RepositoryService(client);
+		commitService = new CommitService(client);
 	}
 
 	public void importGithubComments(String startDate) {
+		// Get all activities since 'startDate' w/ github user name and no comments
+		ArrayList<ActivityEventModel> eventList = sqlDb.getEventsWithNoComments(startDate);
+		String lastGithubUser = "";
+		List<Repository> repoList = null;
+
+		for (int i = 0; i < eventList.size(); i++) {
+			// Get commit info from DB for each student/date combo
+			ActivityEventModel event = eventList.get(i);
+			String gitUser = event.getGithubName();
+
+			try {
+				if (!gitUser.equals(lastGithubUser)) {
+					// New github user, need to get new repo array
+					lastGithubUser = gitUser;
+					repoList = repoService.getRepositories(gitUser);
+					if (repoList.size() == 0)
+						continue;
+
+				} else if (repoList.size() == 0) {
+					// This git account does not exist!!
+					continue;
+				}
+
+				// Update all user comments in this repo list
+				if (updateUserGithubComments(gitUser, startDate, eventList, repoList) == -1)
+					// Rate limit exceeded, so abort
+					break;
+
+			} catch (IOException e) {
+				if (e.getMessage().startsWith("API rate limit exceeded")) {
+					// Rate limit exceeded, so abort
+					JOptionPane.showConfirmDialog(null,
+							"Aborting Github import: Github API rate limit exceeded.\nPlease wait 1 hour and try again.");
+					break;
+
+				} else {
+					sqlDb.getDbLogData().add(new LogDataModel(LogDataModel.GITHUB_IMPORT_FAILURE,
+							event.getStudentNameModel(), event.getClientID(), " for user '" + gitUser + "'"));
+				}
+			}
+		}
+	}
+
+	private int updateUserGithubComments(String githubUser, String startDate, List<ActivityEventModel> eventList,
+			List<Repository> repoList) {
+		try {
+			for (int i = 0; i < repoList.size(); i++) {
+				Repository repo = repoList.get(i);
+				if (repo.getSize() == 0) {
+					continue;
+				}
+
+				// Get all the commits for this repo
+				List<RepositoryCommit> commitList = commitService.getCommits(repo);
+				for (int j = 0; j < commitList.size(); j++) {
+					RepositoryCommit commit = commitList.get(j);
+					long commitDateLong = commit.getCommit().getCommitter().getDate().getTime();
+					String commitDate = new DateTime(commitDateLong).toString("yyyy-MM-dd");
+					String message = commit.getCommit().getMessage();
+
+					if (commitDate.compareTo(startDate) >= 0) {
+						// Find gituser & date match in event list; append multiple comments
+						for (int k = 0; k < eventList.size(); k++) {
+							ActivityEventModel event = eventList.get(k);
+							if (commitDate.equals(event.getServiceDateString())
+									&& githubUser.equals(event.getGithubName())) {
+								// Trim github message to get only summary data
+								int idx = message.indexOf("\n");
+								if (idx > -1)
+									message = message.trim().substring(0, idx);
+
+								// Update comments & repo name, continue to next commit
+								event.setGithubComments(message);
+								sqlDb.updateActivity(event.getClientID(), event.getStudentNameModel(), commitDate,
+										repo.getName(), event.getGithubComments());
+								break;
+							}
+						}
+					}
+				}
+			}
+
+		} catch (IOException e) {
+			if (e.getMessage().startsWith("API rate limit exceeded")) {
+				// Rate limit exceeded, so abort
+				JOptionPane.showConfirmDialog(null,
+						"Aborting Github import: Github API rate limit exceeded.\nPlease wait 1 hour and try again.");
+				return -1;
+			}
+			// TODO: Figure out how to get more info for this error
+			sqlDb.getDbLogData().add(new LogDataModel(LogDataModel.GITHUB_IMPORT_FAILURE,
+					null, 0, " for user '" + githubUser + "': " + e.getMessage()));
+		}
+		return 0;
+	}
+
+	public void importGithubComments_orig(String startDate) {
 		// Get all activities since 'startDate' w/ github user name and no comments
 		ArrayList<ActivityEventModel> eventList = sqlDb.getEventsWithNoComments(startDate);
 		String lastGithubUser = "";
