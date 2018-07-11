4a2f906a210465980a119b05a1c3c7fe9a3e33a9
wavis421 <wendy.avis@jointheleague.org>
2018-07-11 00:27:08 -0700

    Create DatePicker class.

---
+++ src/gui/DatePicker.java
@@ -0,0 +1,63 @@
+package gui;
+
+import java.awt.Dimension;
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Properties;
+
+import javax.swing.JFormattedTextField.AbstractFormatter;
+
+import org.jdatepicker.impl.JDatePanelImpl;
+import org.jdatepicker.impl.JDatePickerImpl;
+import org.jdatepicker.impl.UtilCalendarModel;
+import org.joda.time.DateTime;
+
+public class DatePicker {
+
+	JDatePickerImpl datePicker;
+
+	public DatePicker(DateTime date) {
+		// Create date model & panel
+		Calendar myDate = Calendar.getInstance();
+		myDate.set(Calendar.DAY_OF_MONTH, date.getDayOfMonth());
+		myDate.set(Calendar.MONTH, date.getMonthOfYear() - 1);
+		myDate.set(Calendar.YEAR, date.getYear());
+
+		UtilCalendarModel dateModel = new UtilCalendarModel(myDate);
+		Properties prop = new Properties();
+		JDatePanelImpl datePanel;
+
+		prop.put("text.today", date.getDayOfMonth());
+		prop.put("text.month", date.getMonthOfYear() - 1);
+		prop.put("text.year", date.getYear());
+
+		datePanel = new JDatePanelImpl(dateModel, prop);
+		datePicker = new JDatePickerImpl(datePanel, new DateLabelFormatter());
+		datePicker.setFont(CustomFonts.TABLE_TEXT_FONT);
+		datePicker.setPreferredSize(new Dimension(130, 32));
+	}
+
+	public JDatePickerImpl getDatePicker() {
+		return datePicker;
+	}
+
+	// Nested class for formatting date picker
+	private class DateLabelFormatter extends AbstractFormatter {
+		private static final String datePattern = "yyyy-MM-dd";
+
+		@Override
+		public String stringToValue(String text) throws ParseException {
+			DateTime date = new DateTime(text);
+			return date.toString(datePattern);
+		}
+
+		@Override
+		public String valueToString(Object value) throws ParseException {
+			if (value != null) {
+				DateTime date = new DateTime(value);
+				return date.toString(datePattern);
+			}
+			return "";
+		}
+	}
+}
--- src/gui/SelectDateRangeDialog.java
+++ src/gui/SelectDateRangeDialog.java
@@ -5,30 +5,24 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.text.ParseException;
-import java.util.Calendar;
-import java.util.Properties;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
-import javax.swing.JFormattedTextField.AbstractFormatter;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
 
-import org.jdatepicker.impl.JDatePanelImpl;
 import org.jdatepicker.impl.JDatePickerImpl;
-import org.jdatepicker.impl.UtilCalendarModel;
 import org.joda.time.DateTime;
 
 import model.DateRangeEvent;
 
 public class SelectDateRangeDialog extends JDialog {
 	private static final int DIALOG_WIDTH = 300;
-	private static final int DIALOG_HEIGHT = 180;
+	private static final int DIALOG_HEIGHT = 190;
 
 	// Private instance variables
 	private DateRangeEvent dateRangeEvent;
@@ -66,10 +60,10 @@ public class SelectDateRangeDialog extends JDialog {
 		okButton.setPreferredSize(btnSize);
 
 		// Add components to panels
-		startDatePicker = createDatePicker(startDate);
+		startDatePicker = new DatePicker(startDate).getDatePicker();
 		startDatePanel.add(startDateLabel);
 		startDatePanel.add(startDatePicker);
-		endDatePicker = createDatePicker(endDate);
+		endDatePicker = new DatePicker(endDate).getDatePicker();
 		endDatePanel.add(endDateLabel);
 		endDatePanel.add(endDatePicker);
 
@@ -120,48 +114,5 @@ public class SelectDateRangeDialog extends JDialog {
 
 	public DateRangeEvent getDialogResponse() {
 		return dateRangeEvent;
-	}
-
-	private JDatePickerImpl createDatePicker(DateTime date) {
-		// Create date model & panel
-		Calendar myDate = Calendar.getInstance();
-		myDate.set(Calendar.DAY_OF_MONTH, date.getDayOfMonth());
-		myDate.set(Calendar.MONTH, date.getMonthOfYear() - 1);
-		myDate.set(Calendar.YEAR, date.getYear());
-
-		UtilCalendarModel dateModel = new UtilCalendarModel(myDate);
-		Properties prop = new Properties();
-		JDatePanelImpl datePanel;
-
-		prop.put("text.today", date.getDayOfMonth());
-		prop.put("text.month", date.getMonthOfYear() - 1);
-		prop.put("text.year", date.getYear());
-
-		datePanel = new JDatePanelImpl(dateModel, prop);
-		JDatePickerImpl datePicker = new JDatePickerImpl(datePanel, new DateLabelFormatter());
-		datePicker.setFont(CustomFonts.TABLE_TEXT_FONT);
-		datePicker.setPreferredSize(new Dimension(130, 26));
-
-		return datePicker;
-	}
-
-	// Nested class for formatting date picker
-	private class DateLabelFormatter extends AbstractFormatter {
-		private static final String datePattern = "yyyy-MM-dd";
-
-		@Override
-		public String stringToValue(String text) throws ParseException {
-			DateTime date = new DateTime(text);
-			return date.toString(datePattern);
-		}
-
-		@Override
-		public String valueToString(Object value) throws ParseException {
-			if (value != null) {
-				DateTime date = new DateTime(value);
-				return date.toString(datePattern);
-			}
-			return "";
-		}
 	}
 }
